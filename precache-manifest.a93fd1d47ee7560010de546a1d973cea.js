85e8579518e2978158cfac0a242a9d39953ce999
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-26 05:46:05 +0000

    Deploy aleksei-bulgak-study/RSConf-landing-page to github.com/aleksei-bulgak-study/RSConf-landing-page.git:gh-pages

---
+++ precache-manifest.a93fd1d47ee7560010de546a1d973cea.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "06a7717031932522a83c",
+    "url": "/static/js/main.06a77170.chunk.js"
+  },
+  {
+    "revision": "0e77ea556af3fbf3c6ef",
+    "url": "/static/js/1.0e77ea55.chunk.js"
+  },
+  {
+    "revision": "394fa31a878a8ffc0ffd3e3fef6ab449",
+    "url": "/index.html"
+  }
+];
