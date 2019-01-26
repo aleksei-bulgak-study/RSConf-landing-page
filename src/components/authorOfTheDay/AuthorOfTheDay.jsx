7c1c7236b68e0271032b3c1232236e3606559e02
Dekim12 <Dekim12@github.com>
2019-01-26 16:15:02 +0200

    feat:create AuthorOfTheDay component and realize defineAuthors function

--- src/components/authorOfTheDay/AuthorOfTheDay.jsx
+++ src/components/authorOfTheDay/AuthorOfTheDay.jsx
@@ -2,29 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './authorOfTheDay.css';
 
-const AuthorOfTheDay = ({ autorsInfo }) => {
-  const autorsCount = autorsInfo.length();
+const defineAuthors = (authors) => {
+  const date = new Date();
+  const currentDate = date.getDate();
+  const autorsCount = authors.length;
+  const currentAutorsIndex = currentDate % autorsCount;
+  return authors[currentAutorsIndex - 1];
+};
+
+const AuthorOfTheDay = ({ authorsInfo }) => {
+  const author = defineAuthors(authorsInfo);
+  const authorsPhoto = author.titleImage;
+  const authorsName = `${author.firstName} ${author.lastName}`;
+
   return (
-    <div>
-      <h2>AuthorOfTheDay</h2>
-      <p>AuthorOfTheDay</p>
-      <ol>
-        <li>Nulla pulvinar diam</li>
-        <li>Facilisis bibendum</li>
-        <li>Vestibulum vulputate</li>
-        <li>Eget erat</li>
-        <li>Id porttitor</li>
-      </ol>
+    <div className="autor-of-the-day">
+      <h2>Author of the day</h2>
+      <a href={authorsPhoto}>
+        <img src={authorsPhoto} alt={authorsName} />
+        <span>{authorsName}</span>
+      </a>
     </div>
   );
 };
 
 AuthorOfTheDay.propTypes = {
-  autorsInfo: PropTypes.arrayOf(PropTypes.obj),
+  authorsInfo: PropTypes.arrayOf(PropTypes.obj),
 };
 
 AuthorOfTheDay.defaultProps = {
-  autorsInfo: [],
+  authorsInfo: [],
 };
 
 export default AuthorOfTheDay;
