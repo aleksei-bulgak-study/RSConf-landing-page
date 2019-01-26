c0746324b2f0a92d64103926c5399ff408cffe6f
Dekim12 <Dekim12@github.com>
2019-01-26 12:18:58 +0200

    feat:change AboutUs and create Developer components, add constants folder

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Developer } from '../index';
+import { DEVELOPERS } from '../../constants/constants';
 import './aboutUs.css';
 
-const AboutUs = () => (
-  <div>
-    <h2>AboutUs</h2>
-    <p>AboutUs</p>
-    <ol>
-      <li>Nulla pulvinar diam</li>
-      <li>Facilisis bibendum</li>
-      <li>Vestibulum vulputate</li>
-      <li>Eget erat</li>
-    </ol>
-  </div>
-);
+const AboutUs = () => {
+  const generateDevelopers = devInfo => devInfo.map((dev, index) => {
+    const { avatar, name, nickname } = dev;
+    const key = name.slice(2) + nickname.slice(3) + index;
+    return <Developer avatar={avatar} name={name} nickname={nickname} key={key} />;
+  });
+
+  return (
+    <div className="about-us">
+      <h2>Our project team</h2>
+      <div className="our-developers">{generateDevelopers(DEVELOPERS)}</div>
+    </div>
+  );
+};
 
 export default AboutUs;
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,3 +3,4 @@ export { default as AuthorOfTheDay } from './authorOfTheDay/AuthorOfTheDay';
 export { default as AboutUs } from './aboutUs/AboutUs';
 export { default as Search } from './search/Search';
 export { default as SearchResult } from './searchResult/SearchResult';
+export { default as Developer } from './developer/Developer';
